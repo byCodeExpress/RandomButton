bcf983cebfd44452293e0506136acacb24fa15fe
byCodeExpress <by.eugeniusz@gmail.com>
2016-10-11 15:27:04 +0200

    Add files via upload

---
+++ RandomButton/Zadanie domowe 2.3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_domowe_2._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            var rnd = new Random();
+            var x = rnd.Next();
+            var y = rnd.Next();
+            x = r.Next();
+            y = y.Next();
+            button1.Location = new Point(x, y);
+           
+        }
+    }
+}
